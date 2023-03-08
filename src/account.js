657381a896b721d7f713b15626a48b54cdd749e5
jackson <boulter.jackson@gmail.com>
2023-03-08 14:00:51 -0400

    fixed navigation around site/hrefs in ejs files

--- src/account.js
+++ src/account.js
@@ -11,12 +11,14 @@ const auth = require('./authenticate')
  * routes that handle basic account functionality such as logging in and logging out, also redirects 
  * the user to their 'homepage' when they successfully login
  */
-router.get('/homepage', auth.checkAuthenticated, async(req, res) => { //todo: rename to account/homepage? user/create?
+router.get('/homepage', auth.checkAuthenticated, async(req, res) => { //todo: rename to account/homepage? 
+    res.render("homepage.ejs")
+})
+router.get('/create', auth.checkAuthenticated, async(req, res) => { 
 
     console.log("USER ID IS " + req.user.id)
     res.render("content-create.ejs", { name: req.user.firstName })//TODO: 2nd arg is not currenlty being used on the webpage
 })
-
 
 router.get('/login', auth.checkNotAuthenticated, (req, res)=>{ 
     res.render('login.ejs')
