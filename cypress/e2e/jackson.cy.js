9e7269b0a928ae8b7525e5e6c8198a66f81dcb7d
jackson <boulter.jackson@gmail.com>
2023-03-09 16:08:51 -0400

    added alert to forgotPassword for successful atmpt

---
+++ cypress/e2e/jackson.cy.js
@@ -0,0 +1,16 @@
+describe('forgot password page', () => { 
+    it('email is registered', () => {
+        cy.visit('http://localhost:8080/password/forgot');
+        
+    });
+    it('email is NOT registered', () => {
+        
+    });
+    it('vist reset page from email that is registerd', () => {
+        
+    });
+    it('attempt to visit reset page', () => {
+        
+    });
+
+ })
