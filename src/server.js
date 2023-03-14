a34ab119d90a1f4fdacf97547e96a5e13a0365d8
Ben <blhutchings@upei.ca>
2023-03-14 20:48:54 -0300

    Clean up

--- src/server.js
+++ src/server.js
@@ -1,94 +1,59 @@
+require("dotenv").config()
 
-/**
- * modular dependancies
-*/
 const express = require("express")
-const bcrypt = require("bcrypt")
-const bodyParser  = require("body-parser")
-
-const server = express()
-//const render  = require("ejs") //todo, not sure if this is needed in this file
-const flash = require("express-flash")
 const session = require("express-session")
-
-
-const passport = require("passport")
-//const { check, validationResult } = require("express-validator") //todo, not sure if this is needed in this file
+const flash = require("express-flash")
 const methodOverride = require("method-override")
+const bodyParser  = require("body-parser")
 const expressH5P = require('./h5p/expressH5P')
 
-
-//const sendEmail = require("../utils/email/sendEmail") //todo, not sure if this is needed in this file
-
-const rateLimit = require('express-rate-limit')
-
-const { Prisma } = require("@prisma/client") //TODO: is this needed/being used here, also see database.js
-const { application } = require("express") //TODO: is this being used?
-
-
-
-const ip = require('../utils/getPublicIp')
-const auth = require('./authenticate')
 const regestrationRoute = require('./registration')
 const usersRoute = require('./users')
 const accountRoute = require('./account')
 const passwordRoute = require('./password')
-const initializePassport = require("./config/passport")
+
+const passport = require("passport")
+const initializePassport = require('./config/passport')
 const db = require("./config/database")
+const auth = require('./authenticate')
 
-
-/***
- * Config
- */
-if (process.env.NODE_ENV !== "production") {//checks if we are in prod envoirment
-    require("dotenv").config()
-}
 initializePassport(
     passport,
     async email => await db.User.findFirst({ where: { email } }),
     async id => await db.User.findFirst({ where: { id } })
 )
+
+const server = express()
+
 server.use(session({
     secret: process.env.SESSION_SECRET,
     resave: false, // we want to resave the session variable if nothing is changed
     saveUninitialized: false
 }))
-const limiter = rateLimit({
-    windowMs: 1*60*1000, //1 minute
-    max: 10
-})
-//server.use(limiter) //todo, enable, currently disabled to test something
+
 server.set('views', './views')
 server.set('view engine', 'ejs')
-server.use('/public', express.static('public')) //TODO: may be expressed/condensed as app.use(express.static('public')) see here: https://expressjs.com/en/starter/static-files.html
-server.use(express.urlencoded({ extended: false }))
+server.use('/public', express.static('public'))
 server.use(flash())
+server.use(methodOverride("_method"))
+
 server.use(passport.initialize())
 server.use(passport.session())
-server.use(methodOverride("_method"))
+
+// Express H5P setup
 server.use(bodyParser.json({ limit: '500mb' }));
-server.use(bodyParser.urlencoded({extended: true}));
-
+server.use(
+    bodyParser.urlencoded({
+        extended: true
+    })
+);
 expressH5P(server)
-
-
-
-
-/**
- * start the server and export server module
- */
-const PORT = 8080
-server.listen(PORT)
-console.log(`Server started on port http://localhost:${PORT}...`)
-module.exports = server
-
-
 
 /**
  * server code, primarily uses Expresses routes, and creates 'mini-apps' for the main functionalities
  * of our application
  */
-server.get('/', async(req, res) => {
+server.get('/', async (req, res) => {
     res.render("index.ejs")
 })
 
@@ -97,7 +62,13 @@ server.use('/users', usersRoute)
 server.use('/account', accountRoute)
 server.use('/password', passwordRoute)
 
-//ip() //posts our public IP to the console
+
+/**
+ * start the server and export server module
+ */
+const PORT = process.env.PORT || '8080';
+server.listen(PORT)
+console.log(`Server started on port http://localhost:${PORT}...`)
 
 
 
