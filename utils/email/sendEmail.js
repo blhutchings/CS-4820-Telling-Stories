8410c853d8591ece0fbdfa3d904f137773b031c8
Ben <blhutchings@upei.ca>
2023-04-23 19:56:39 -0300

    Formated email html

--- utils/email/sendEmail.js
+++ utils/email/sendEmail.js
@@ -18,11 +18,13 @@ const sendEmail = async (receivingEmailAddress, emailSubject, payload, ejsTempla
         }
     })
 
+    const html = ejsFile(payload)
+
     let mailOptions = {
         from: process.env.EMAIL,
         to: receivingEmailAddress, 
         subject: emailSubject,
-        html: ejsFile(payload) // this will throw an error if you dont supply every var declared in the .ejs file
+        html: html // this will throw an error if you dont supply every var declared in the .ejs file
     }
 
     
