c7b1e5717860cb0c590c64f76e861655f66ee225
Ben <blhutchings@upei.ca>
2023-04-23 18:56:05 -0300

    Expanded userId middelware

--- src/config/database.js
+++ src/config/database.js
@@ -5,10 +5,12 @@ const prisma = new PrismaClient();
 
 // Required middelware to convert userId from a session into int
 prisma.$use(async (params, next) => {
-    if (params.model == 'Content') {
+    if (params.model == 'Content' && params.action == 'find') {
         if (params.args?.where?.userId) {
             params.args.where.userId = parseInt(params.args.where.userId)
         }
+    } else if (params.model == 'Content' && params.action == 'create') {
+        params.args.data.userId = parseInt(params.args.data.userId)
     }
     // Manipulate params here
     const result = await next(params)
