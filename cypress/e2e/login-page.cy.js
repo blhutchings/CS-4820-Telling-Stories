20242c4b00ea3d1f1cdba3bcd7d05c7d09ed347e
BoulterJackson <54991711+BoulterJackson@users.noreply.github.com>
2023-04-23 13:46:24 -0300

    added tests

--- cypress/e2e/login-page.cy.js
+++ cypress/e2e/login-page.cy.js
@@ -1,4 +1,8 @@
-describe('proper input into forms', () =>{
+/**
+ * this testing spec uses an account created soley for testing purposes
+ * testing@testing.com -- iGm2UALK!Eg@G.
+ */
+describe('various tests', () =>{
   
     it('text is properly inputted into forms', () => {
       cy.visit("http://localhost:8080/account/login")
@@ -9,28 +13,22 @@ describe('proper input into forms', () =>{
       cy.get('#form2Example22').should('have.value','Testingpassword1!')
   
       cy.get('.text-center > .btn').click()
-    });
+    })
     it('invalid email', () => {
       cy.visit("http://localhost:8080/account/login")
       cy.get('#form2Example11').type('thisemailshouldntexist@testing.com')
       cy.get('#form2Example22').type('doesntMatter') 
       
       cy.get('.text-center > .btn').click()
-    });
+      cy.contains('.alert-danger','User is not registered')
+    })
     it('invalid password', () => {
       cy.visit("http://localhost:8080/account/login")
-      cy.get('#form2Example11').type('t@t')
+      cy.get('#form2Example11').type('testing@testing.com')
       cy.get('#form2Example22').type('wrongPassword') 
-      
       cy.get('.text-center > .btn').click()
-    });
+
+      cy.contains('.alert-danger','User is not registered')
+    })
   })
   
-  describe('button functionality', () => {
-    it('index login', () => {
-      cy.visit("http://localhost:8080/")
-      cy.get('.button').click()
-      cy.url().should('include', "/account/login")
-    });
-  
-  })
